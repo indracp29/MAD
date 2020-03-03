347a613677d056e84b7b4e095eaa52081d7e9c4d
indracp29 <53854527+indracp29@users.noreply.github.com>
2020-03-03 15:44:32 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,47 +1,76 @@
-package com.example.fornt_change;
+package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-
+import android.view.View.OnClickListener;
 import android.view.View;
-import android .widget.Button;
-import android .widget.TextView;
-import android .graphics.Color;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    int ch=1;
-    float font=30;
+public class MainActivity extends AppCompatActivity implements OnClickListener{
+    float num1=0,num2=0,ans=0;
+    Button a,s,m,d;
+    EditText n1,n2;
+    TextView result;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView t=(TextView)findViewById(R.id.textView);
-        Button b1= (Button)findViewById(R.id.button);
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                t.setTextSize(font);
-                font = font + 5;
-                if (font == 50)
-                    font = 30;
-            }
-        });
-        Button b2= (Button) findViewById(R.id.button2);
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                switch(ch) {
-                    case 1: t.setTextColor(Color.RED);break;
-                    case 2: t.setTextColor(Color.GREEN);break;
-                    case 3: t.setTextColor(Color.BLUE);break;
-                    case 4: t.setTextColor(Color.CYAN);break;
-                    case 5: t.setTextColor(Color.YELLOW);break;
-                    case 6: t.setTextColor(Color.MAGENTA);break;
-                }
-                ch++;
-                if(ch == 7) ch=1;
-            }  });
+        n1=(EditText) findViewById(R.id.n1);
+        n2=(EditText) findViewById(R.id.n2);
+        a=(Button) findViewById(R.id.add);
+        s=(Button) findViewById(R.id.sub);
+        d=(Button) findViewById(R.id.div);
+        m=(Button) findViewById(R.id.mul);
+        result=(TextView) findViewById(R.id.ans);
+
+        a.setOnClickListener(this);
+        s.setOnClickListener(this);
+        d.setOnClickListener(this);
+        m.setOnClickListener(this);
+
     }
 
+    @Override
+    public void onClick(View v){
+        num1=Float.parseFloat(n1.getText().toString());
+        num2=Float.parseFloat(n2.getText().toString());
+        String op="";
+        switch(v.getId())
+        {
+            case R.id.add:
+            {
+                op="+";
+                ans=num1+num2;
+                break;
+            }
+            case R.id.sub:
+            {
+                op="-";
+                ans=num1-num2;
+                break;
+            }
+            case R.id.mul:
+            {
+                op="*";
+                ans=num1*num2;
+                break;
+            }
+            case R.id.div:
+            {
+                op="/";
+                ans=num1/num2;
+                break;
+            }
+            default:
+                result.setText("");
+
+        }
+
+        result.setText(num1+" " +op+" "+num2+"="+ans);
+    }
 }
